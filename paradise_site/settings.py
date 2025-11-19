cfe770449fe2272929efb1cc9a5c7b6184fdf917
Nico Haugaard <nicohaugaard@gmail.com>
2025-11-19 16:03:49 -0300

    Fix render deployment

--- paradise_site/settings.py
+++ paradise_site/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'paradise_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "Templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
